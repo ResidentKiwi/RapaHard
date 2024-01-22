e8c8e66e3c49d7fadd26f4aec46889c7b5f01724
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2024-01-22 19:47:11 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,9 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
 
-...
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
