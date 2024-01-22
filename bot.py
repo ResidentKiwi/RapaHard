04407428377900b8cc53dc372ffc880127b5115a
Residential Kiwi <113327928+ResidentKiwi@users.noreply.github.com>
2024-01-22 19:56:51 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,20 +6,6 @@ from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import InlineQueryHandler
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-    
-if __name__ == '__main__':
-    ...
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-
-    application.run_polling()
     
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -35,4 +21,17 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     
+    application.run_polling()
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    
+if __name__ == '__main__':
+    ...
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+
     application.run_polling()
